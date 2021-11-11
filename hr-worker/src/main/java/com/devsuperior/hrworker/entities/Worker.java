96c4096944caf741361896ebe00862a9c8d95711
walacetalesfs <walacetales@hotmail.com>
2021-11-10 23:35:19 -0300

    worker implementation

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,30 @@
+package com.devsuperior.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+}
