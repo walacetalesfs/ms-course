79432608112dff3ce2218c2ceac3b930792c73dd
walacetalesfs <walacetales@hotmail.com>
2021-11-10 23:34:52 -0300

    payroll implementation

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -0,0 +1,24 @@
+package com.devsuperior.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Payment implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+    public Double getTotal() {
+        return days * dailyIncome;
+    }
+}
---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,20 @@
+package com.devsuperior.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+}
