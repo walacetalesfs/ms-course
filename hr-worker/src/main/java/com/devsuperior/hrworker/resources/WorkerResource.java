0af9b4ebb9f02955d2ed78c2fee8f4c16526a128
walacetalesfs <walacetales@hotmail.com>
2021-11-22 23:56:25 -0300

    configurando config server e client para projeto worker

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,9 +20,6 @@ public class WorkerResource {
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
     @Autowired
-    private Environment env;
-
-    @Autowired
     private WorkerRepository repository;
 
     @GetMapping
@@ -34,9 +30,6 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
-
-        logger.info("PORT = " + env.getProperty("local.server.port"));
-
         Worker ret = repository.findById(id).get();
         return ResponseEntity.ok(ret);
     }
