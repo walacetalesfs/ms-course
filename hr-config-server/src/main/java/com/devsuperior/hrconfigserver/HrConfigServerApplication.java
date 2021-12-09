74fcb4651277e7a0f26b9088b7effc767eae050f
walacetalesfs <walacetales@hotmail.com>
2021-12-09 19:03:03 -0300

    configs docker/deploy

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -3,11 +3,23 @@ package com.devsuperior.hrconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/config-server")
 public class HrConfigServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HrConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication.class, args);
+    }
+
+    @GetMapping(value = "/health")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok("");
+    }
 }
--- hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -3,13 +3,23 @@ package com.devsuperior.hreurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/eureka-server")
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
 
+	@GetMapping(value = "/health")
+	public ResponseEntity<String> getHealth() {
+		return ResponseEntity.ok("");
+	}
 }
